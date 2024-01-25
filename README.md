9ae74da4f341647d2829c9ad3b356a7325d0be02
fatiharge <fatih@fatiharge.com>
2024-01-25 22:35:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# builder_app_template
+
+
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/fatiharge/builder_app_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fatiharge.builder_app_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
